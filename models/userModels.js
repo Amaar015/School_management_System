35eeff4e4756e5f576ea16f96cae80b696a21cce
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2023-02-27 11:26:40 +0500

    update register controller

---
+++ models/userModels.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is Required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is Required"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is Required"]
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    isStudent: {
+        type: Boolean,
+        default: false,
+    },
+    isTeacher: {
+        type: Boolean,
+        default: false,
+    },
+    notification: {
+        type: Array,
+        dafault: []
+    },
+    SeenNotification: {
+        type: Array,
+        dafault: [],
+    }
+})
+
+// create a database Model
+const userModel = mongoose.model('userdb', userSchema)
+// exports a userModel
+module.exports = userModel;
