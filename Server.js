8882189be9ff13fab8bb9e272ac7fad751ab0441
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2023-03-20 10:52:03 +0500

    updated

--- Server.js
+++ Server.js
@@ -2,21 +2,26 @@ const express = require('express');
 const colors = require('colors')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
+const cors = require('cors')
 dotenv.config()
 // Mongo DB connection
 
-require('./config/db.js')
+require('./config/db')
 
 // rest obj
 const app = express();
 
 
 // midleware
+app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 
 // routes
-app.use('/api/v1/user', require('./routes/userRoutes'))
+app.use('/api/v1/teacher', require('./routes/teacherRoutes'))
+app.use('/api/v1/student', require('./routes/studentRoutes'))
+app.use('/api/v1/admin', require('./routes/adminRoutes'))
+
 //listening
 
 const port = process.env.PORT || 8080
